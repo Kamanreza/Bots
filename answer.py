ca0152194f020aa20a74531d55f52981854a07a5
Kamanreza <126678260+Kamanreza@users.noreply.github.com>
2024-10-07 21:13:14 +0330

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,9 +1,9 @@
 {
   "answer": {
-    "choice_1": 9,
-    "choice_2": 4,
-    "choice_3": 10,
-    "choice_4": 2
+    "choice_1": 12,
+    "choice_2": 3,
+    "choice_3": 14,
+    "choice_4": 5
   },
-  "expires": 1726833600
+  "expires": 1728388800
 }
