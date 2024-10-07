a7cb3abacbd6bdd74cc56e6d7c9f09e33e4f04ab
Kamanreza <126678260+Kamanreza@users.noreply.github.com>
2024-10-07 21:17:55 +0330

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,4 +1,11 @@
 {
+  "youtube": {
+    "Watch YouTube Video #1": "070624",
+    "Major Games #1": "070624",
+    "Watch YouTube Shorts #3": "070624",
+    "Watch YouTube Shorts #4": "159390",
+    "Watch YouTube Shorts #6": "241086"
+  },
   "answer": {
     "choice_1": 12,
     "choice_2": 3,
